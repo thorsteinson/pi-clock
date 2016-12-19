35b56c7699337d8a002e9f6c1c7c676695acc0d0
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-12-19 14:04:56 -0800

    Removes compiled files

--- dist/out/pi_clock/clock.cljs
+++
@@ -1,7 +0,0 @@
-(ns pi-clock.clock)
-
-(defn get-time! []
-  (let [time (js/Date.)] 
-    {:hours (.getHours time)
-     :minutes (.getMinutes time)
-     :seconds (.getSeconds time)}))
--- dist/out/pi_clock/clock.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.9.293 {:target :nodejs}
-goog.provide('pi_clock.clock');
-goog.require('cljs.core');
-pi_clock.clock.get_time_BANG_ = (function pi_clock$clock$get_time_BANG_(){
-var time = (new Date());
-return new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"hours","hours",58380855),time.getHours(),new cljs.core.Keyword(null,"minutes","minutes",1319166394),time.getMinutes(),new cljs.core.Keyword(null,"seconds","seconds",-445266194),time.getSeconds()], null);
-});
-
-//# sourceMappingURL=clock.js.map
