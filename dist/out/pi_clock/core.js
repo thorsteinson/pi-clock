ee340af83c46b021148c76e418cc2e5d4d80cde4
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-12-18 23:05:30 -0800

    Adds compiled change

--- dist/out/pi_clock/core.js
+++ dist/out/pi_clock/core.js
@@ -30,7 +30,7 @@ cljs.core.map.call(null,pi_clock.hardware.open_pin_BANG_,pi_clock.core.HOURS_REG
 
 cljs.core.map.call(null,pi_clock.hardware.set_high_BANG_,pi_clock.core.HOURS_REGISTER);
 
-return setTimout((function (){
+return setTimeout((function (){
 return cljs.core.println.call(null,"plz change",(1000));
 }));
 });
